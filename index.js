abf5ca8c0359b82b5f32dce7f7389cc7c79f0556
ShivyaDahiya <103512329+ShivyaDahiya@users.noreply.github.com>
2022-07-01 15:01:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1=Math.random();
 randomNumber1=Math.floor(randomNumber1*6)+1;
-var imgdisplay1="images/dice" + randomNumber1 + ".png";
+var imgdisplay1="dice" + randomNumber1 + ".png";
 document.querySelector("img.imgl").setAttribute("src", imgdisplay1);
 
 var randomNumber2=Math.random();
 randomNumber2=Math.floor(randomNumber2*6)+1;
-var imgdisplay2="images/dice"+randomNumber2+".png";
+var imgdisplay2="dice"+randomNumber2+".png";
 document.querySelector("img.imgr").setAttribute("src", imgdisplay2);
 
 if(randomNumber1>randomNumber2)
